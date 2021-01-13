9e104c23f38e948460ea34802981a94322fb8978
Julian Harrington <jrayharrington@gmail.com>
2021-01-13 12:30:19 +1100

    Move logic to controller, add box shadow to container

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:question]
+    @question = params[:question]
+
+    if @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
